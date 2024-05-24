f72870a905cb401cbda5b719eaeb5366d3494560
kamin <m7mkmnn@gmail.com>
2024-05-24 16:32:10 +0900

    toast_controllerの追加

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import Toast_controller from "./toast_controller";
+application.register('toast', Toast_controller)
