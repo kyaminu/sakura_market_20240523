6dc7c2754f24ccab91e0bc2a297d5529c8ca33cf
kamin <m7mkmnn@gmail.com>
2024-05-24 14:51:19 +0900

    ルーティングの記述

--- config/routes/administrators.rb
+++ config/routes/administrators.rb
@@ -3,5 +3,6 @@ devise_for :administrators, controllers: {
 }
 
 namespace :administrators do
+  resources :administrator, only: %i[index new edit create update destroy]
   root to: 'items#index'
 end
