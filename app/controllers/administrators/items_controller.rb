a3e93b7f946844b38debb9a7145d06d06e78f4a1
kamin <m7mkmnn@gmail.com>
2024-05-26 14:45:36 +0900

    コントローラーの記述

--- app/controllers/administrators/items_controller.rb
+++ app/controllers/administrators/items_controller.rb
@@ -1,4 +1,50 @@
 class Administrators::ItemsController < Administrators::ApplicationController
   def index
+    @items = Item.default_order
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def edit
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to administrators_items_path, notice: '登録しました'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to administrators_items_path, notice: '更新しました'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @item.destroy!
+    redirect_to administrators_items_path, notice: '削除しました', status: :see_other
+  end
+
+  def move_higher
+    @item.move_higher
+    redirect_to administrators_items_path, notice: '順番を変更しました'
+  end
+
+  def move_lower
+    @item.move_lower
+    redirect_to administrators_items_path, notice: '順番を変更しました'
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(%i[name description price_excluding_tax published])
   end
 end
