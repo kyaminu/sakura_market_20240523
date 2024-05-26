66dee9c6aa24f3aeeecc60d41ea5fd7dbcbffd4c
kamin <m7mkmnn@gmail.com>
2024-05-26 14:37:01 +0900

    モデルの記述

---
+++ app/models/item.rb
@@ -0,0 +1,18 @@
+class Item < ApplicationRecord
+  acts_as_list
+
+  has_one_attached :image do |attachable|
+    attachable.variant(:thumb, resize_to_fill: [120, 80])
+    attachable.variant(:full, resize_to_fit: [400, 300])
+  end
+
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :image, presence: true
+  validates :price_excluding_tax, presence: true, numericality: { greater_than_or_equal_to: 1 }
+  validates :position, presence: true
+  validates :tax_rate, presence: true
+
+  scope :default_order, -> { order(:position) }
+  scope :published, -> { where(published: true) }
+end
