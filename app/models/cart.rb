7e4ebf6fa138205ef98303c5f37f187559a9882f
kamin <m7mkmnn@gmail.com>
2024-05-28 17:04:05 +0900

    feat: cartモデルの作成

---
+++ app/models/cart.rb
@@ -0,0 +1,5 @@
+class Cart < ApplicationRecord
+  belongs_to :user
+
+  validates :user_id, uniqueness: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_28_043833) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_28_074640) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_28_043833) do
     t.index ["reset_password_token"], name: "index_administrators_on_reset_password_token", unique: true
   end
 
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id", unique: true
+  end
+
   create_table "items", force: :cascade do |t|
     t.string "name", null: false
     t.string "description", null: false
@@ -81,4 +88,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_28_043833) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "carts", "users"
 end
