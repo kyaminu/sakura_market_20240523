b294df9eaea37fac932bb13fa719a81234b2e782
kamin <m7mkmnn@gmail.com>
2024-05-28 17:26:32 +0900

    feat: ルーティングの追加

--- config/routes/user.rb
+++ config/routes/user.rb
@@ -3,5 +3,7 @@ devise_for :users, controllers: {
 }
 
 resources :items, only: %i[show]
+resource :cart, only: %i[show]
+resources :cart_items, only: %i[create update destroy]
 
 root to: 'items#index'
