fde39c198a934b6c73dcc67748b6a2985f51085b
kamin <m7mkmnn@gmail.com>
2024-05-30 15:31:55 +0900

    feat: カートに追加ボタンの実装

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,4 +5,8 @@ class Cart < ApplicationRecord
   def subtotal
     cart_items.sum { |cart_item| cart_item.item.price_included_tax * cart_item.quantity }
   end
+
+  def is_in?(item)
+    cart_items.exists?(item_id: item)
+  end
 end
