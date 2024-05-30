c4baf05a813f0bc3c201780ad38c2826a0346aaf
kamin <m7mkmnn@gmail.com>
2024-05-30 22:32:52 +0900

    refactor: include?に変更

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -6,7 +6,7 @@ class Cart < ApplicationRecord
     cart_items.sum { |cart_item| cart_item.item.price_included_tax * cart_item.quantity }
   end
 
-  def is_in?(item)
+  def include?(item)
     cart_items.exists?(item_id: item)
   end
 end
