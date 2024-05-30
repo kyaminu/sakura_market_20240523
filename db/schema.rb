70b8e4bce3b7e8ee41020e17241a54ffdf40cec7
kamin <m7mkmnn@gmail.com>
2024-05-30 14:48:57 +0900

    feat: cartモデルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_29_122735) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_30_054651) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_29_122735) do
     t.index ["reset_password_token"], name: "index_administrators_on_reset_password_token", unique: true
   end
 
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id", unique: true
+  end
+
   create_table "items", force: :cascade do |t|
     t.string "name", null: false
     t.string "description", null: false
@@ -96,4 +103,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_29_122735) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "addresses", "users"
+  add_foreign_key "carts", "users"
 end
