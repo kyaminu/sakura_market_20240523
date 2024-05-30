6545e5d033de0bf9be5ab98b6f2f915eebb28d70
kamin <m7mkmnn@gmail.com>
2024-05-30 15:20:23 +0900

    feat: 税込価格で小計が出るように

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,6 +3,6 @@ class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
 
   def subtotal
-    cart_items.sum { |cart_item| cart_item.item.price_excluding_tax * cart_item.item.tax_rate * cart_item.quantity }
+    cart_items.sum { |cart_item| cart_item.item.price_included_tax * cart_item.quantity }
   end
 end
