36d0dbca4648a572d459903653c740f24c809054
kamin <m7mkmnn@gmail.com>
2024-05-30 22:34:33 +0900

    refactor: cartとuserのユニークバリデーションの追加

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,6 +2,8 @@ class Cart < ApplicationRecord
   belongs_to :user, optional: true
   has_many :cart_items, dependent: :destroy
 
+  validates :user_id, uniqueness: true
+
   def subtotal
     cart_items.sum { |cart_item| cart_item.item.price_included_tax * cart_item.quantity }
   end
