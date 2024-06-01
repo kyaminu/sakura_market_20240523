3f820a113d4300b6a861ea6c57ee8de2d6fe4c44
kamin <m7mkmnn@gmail.com>
2024-06-01 23:03:07 +0900

    refactor: cartのindex削除

--- db/migrate/20240530054948_create_cart_items.rb
+++ db/migrate/20240530054948_create_cart_items.rb
@@ -1,7 +1,7 @@
 class CreateCartItems < ActiveRecord::Migration[7.1]
   def change
     create_table :cart_items do |t|
-      t.references :cart, null: false, foreign_key: true
+      t.references :cart, null: false, foreign_key: true, index: false
       t.references :item, null: false, foreign_key: true
       t.integer :quantity, null: false, default: 1
 
