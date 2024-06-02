125091f8cbd2f2d7c2bb3a8d6cf5278e359c701a
kamin <m7mkmnn@gmail.com>
2024-06-02 17:33:47 +0900

    feat: カートマージ機能の実装

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,20 @@
 class Users::SessionsController < Devise::SessionsController
+  def create
+    super
+
+    session_cart = Cart.find(session[:cart_id])
+    session_cart.cart_items.each do |session_item|
+      user_cart_item = current_user.cart.cart_items.find_by(item_id: session_item.item_id)
+
+      if user_cart_item
+        user_cart_item.quantity += session_item.quantity
+        user_cart_item.save
+      else
+        current_user.cart.cart_items << CartItem.new(item_id: session_item.item_id, quantity: session_item.quantity)
+      end
+    end
+  end
+
   def destroy
     super
     reset_session
