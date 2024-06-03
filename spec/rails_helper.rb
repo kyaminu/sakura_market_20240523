401658572947686ad6c5cbdb5f25ae0201fee783
kamin <m7mkmnn@gmail.com>
2024-06-03 23:46:52 +0900

    test: no headlessでテストできるように

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,7 +64,4 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::IntegrationHelpers, type: :system
-  config.before(:each, type: :system) do
-    driven_by :selenium_chrome_headless
-  end
 end
