1d76826cfc24b583eac2916658cd794fcf86b849
kamin <m7mkmnn@gmail.com>
2024-06-03 17:06:19 +0900

    refactor: putsの削除

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Users::SessionsController < Devise::SessionsController
   def create
     super
-    puts 'ログインしたよ'
+
     session_cart = Cart.find(session[:cart_id])
     current_user.merge_cart(session_cart)
   end
