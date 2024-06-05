de883dca066ae6c06642ebf126851d53af36b474
kamin <m7mkmnn@gmail.com>
2024-06-05 14:47:03 +0900

    feat: コントローラの記述

---
+++ app/controllers/users/purchases_controller.rb
@@ -0,0 +1,20 @@
+class Users::PurchasesController < Users::ApplicationController
+  def new
+    @purchase = current_user.purchases.new
+  end
+
+  def create
+    @purchase = current_user.purchases.new(purchase_params)
+    if @purchase.save
+      # TODO: まだ履歴画面作ってないので一旦ここに返す
+      redirect_to root_path, notice: '商品を購入しました。'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def purchase_params
+      params.require(:purchase).permit(%i[delivery_fee handling_fee delivery_on delivery_time name name postal_code address])
+    end
+end
