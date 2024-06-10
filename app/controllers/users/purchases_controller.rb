9497a84bbc22a3790ef9b73b4d642bbb27c13556
kamin <m7mkmnn@gmail.com>
2024-06-10 22:21:12 +0900

    refactor: リダイレクト先のパスを変更

--- app/controllers/users/purchases_controller.rb
+++ app/controllers/users/purchases_controller.rb
@@ -15,8 +15,7 @@ class Users::PurchasesController < Users::ApplicationController
   def create
     @purchase = current_user.purchases.build(purchase_params)
     if @purchase.purchase_items_from_cart(current_cart)
-      # TODO: まだ履歴画面作ってないので一旦ここに返す
-      redirect_to root_path, notice: '商品を購入しました。'
+      redirect_to users_purchases_path, notice: '商品を購入しました。'
     else
       render :new, status: :unprocessable_entity
     end
