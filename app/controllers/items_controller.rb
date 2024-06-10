c41cc2f4ab3d09eeb2bd892b99b9583060adb572
kamin <m7mkmnn@gmail.com>
2024-06-10 14:16:10 +0900

    refactor: 商品が見つからなくても404出す体なので、RecordNotFoundは削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,8 +5,5 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.published.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    flash[:notice] = "この商品は現在取り扱っておりません"
-    redirect_back(fallback_location: root_path)
   end
 end
