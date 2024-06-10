5cfb7b853a79c7ce418e27d31a58195b4c6020a4
kamin <m7mkmnn@gmail.com>
2024-06-10 22:37:33 +0900

    test: each文使ってpurchase_item作成

--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -19,12 +19,11 @@ describe Purchase do
       item4 = create(:item)
       item5 = create(:item)
       item6 = create(:item)
-      create(:purchase_item, purchase:, item_id: item1.id, item_name: item1.name, item_description: item1.description, item_price_excluding_tax: item1.price_excluding_tax)
-      create(:purchase_item, purchase:, item_id: item2.id, item_name: item2.name, item_description: item2.description, item_price_excluding_tax: item2.price_excluding_tax)
-      create(:purchase_item, purchase:, item_id: item3.id, item_name: item3.name, item_description: item3.description, item_price_excluding_tax: item3.price_excluding_tax)
-      create(:purchase_item, purchase:, item_id: item4.id, item_name: item4.name, item_description: item4.description, item_price_excluding_tax: item4.price_excluding_tax)
-      create(:purchase_item, purchase:, item_id: item5.id, item_name: item5.name, item_description: item5.description, item_price_excluding_tax: item5.price_excluding_tax)
-      create(:purchase_item, purchase:, item_id: item6.id, item_name: item6.name, item_description: item6.description, item_price_excluding_tax: item6.price_excluding_tax)
+
+      items = [item1, item2, item3, item4, item5, item6]
+      items.each do |item|
+        create(:purchase_item, purchase:, item_id: item.id, item_name: item.name, item_description: item.description, item_price_excluding_tax: item.price_excluding_tax)
+      end
 
       expect(purchase.delivery_fee_value).to eq 1320
     end
