a64d6b4caaeebff27dfe0be288de8b17b9446c3b
kamin <m7mkmnn@gmail.com>
2024-06-10 21:56:31 +0900

    test: モデルのテスト記述

--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -1,12 +1,9 @@
-require 'rails_helper'
-
 describe Purchase do
   describe '#delivery_fee_value' do
     it '送料は5商品ごとに660円追加されること' do
       user = create(:user)
       cart = create(:cart, user:)
       address = create(:address, user:)
-
       item = create(:item)
       create(:cart_item, cart:, item:)
       purchase = build(:purchase, user:, delivery_on: 3.business_days.after(Date.today).to_date, address_id: address.id)
@@ -33,12 +30,77 @@ describe Purchase do
   end
 
   describe '#handling_fee_value' do
+    describe '手数料が小計の金額によって変わること' do
+      it '1万円未満は330円になること' do
+        user = create(:user)
+        cart = create(:cart, user:)
+        address = create(:address, user:)
+        item = create(:item, price_excluding_tax: 5000)
+        create(:cart_item, cart:, item:)
+        purchase = build(:purchase, user:, delivery_on: 3.business_days.after(Date.today).to_date, address_id: address.id)
+        purchase.address = address.full_address
+        create(:purchase_item, purchase:, item_id: item.id, item_name: item.name, item_description: item.description, item_price_excluding_tax: item.price_excluding_tax)
+
+        expect(purchase.handling_fee_value).to eq 330
+      end
+
+      it '1万円以上、3万未満は440円になること' do
+        user = create(:user)
+        cart = create(:cart, user:)
+        address = create(:address, user:)
+        item = create(:item, price_excluding_tax: 15000)
+        create(:cart_item, cart:, item:)
+        purchase = build(:purchase, user:, delivery_on: 3.business_days.after(Date.today).to_date, address_id: address.id)
+        purchase.address = address.full_address
+        create(:purchase_item, purchase:, item_id: item.id, item_name: item.name, item_description: item.description, item_price_excluding_tax: item.price_excluding_tax)
+
+        expect(purchase.handling_fee_value).to eq 440
+      end
+
+      it '3万円以上、10万未満は660円になること' do
+        user = create(:user)
+        cart = create(:cart, user:)
+        address = create(:address, user:)
+        item = create(:item, price_excluding_tax: 35000)
+        create(:cart_item, cart:, item:)
+        purchase = build(:purchase, user:, delivery_on: 3.business_days.after(Date.today).to_date, address_id: address.id)
+        purchase.address = address.full_address
+        create(:purchase_item, purchase:, item_id: item.id, item_name: item.name, item_description: item.description, item_price_excluding_tax: item.price_excluding_tax)
+
+        expect(purchase.handling_fee_value).to eq 660
+      end
+
+      it '10万円以上は1100円になること' do
+        user = create(:user)
+        cart = create(:cart, user:)
+        address = create(:address, user:)
+        item = create(:item, price_excluding_tax: 120000)
+        create(:cart_item, cart:, item:)
+        purchase = build(:purchase, user:, delivery_on: 3.business_days.after(Date.today).to_date, address_id: address.id)
+        purchase.address = address.full_address
+        create(:purchase_item, purchase:, item_id: item.id, item_name: item.name, item_description: item.description, item_price_excluding_tax: item.price_excluding_tax)
+
+        expect(purchase.handling_fee_value).to eq 1100
+      end
+    end
   end
 
   describe '#total_value' do
-  end
+    it '合計金額が表示されること' do
+      user = create(:user)
+      cart = create(:cart, user:)
+      address = create(:address, user:)
+      item = create(:item, price_excluding_tax: 100)
+      another_item = create(:item, price_excluding_tax: 200)
+      cart_item = create(:cart_item, cart:, item:, quantity: 2)
+      another_cart_item = create(:cart_item, cart:, item: another_item, quantity: 1)
+      purchase = build(:purchase, user:, delivery_on: 3.business_days.after(Date.today).to_date, address_id: address.id)
+      purchase.address = address.full_address
+      create(:purchase_item, purchase:, item_id: item.id, item_name: item.name, item_description: item.description, item_price_excluding_tax: item.price_excluding_tax, quantity: cart_item.quantity)
+      create(:purchase_item, purchase:, item_id: another_item.id, item_name: another_item.name, item_description: another_item.description, item_price_excluding_tax: another_item.price_excluding_tax, quantity: another_cart_item.quantity)
 
-  describe '#delivery_fee_value' do
+      expect(purchase.total_value).to eq 1422
+    end
   end
 end
 
