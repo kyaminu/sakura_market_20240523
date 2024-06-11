199990dedcb25851938ddd65bdd1a3aa0719124c
kamin <m7mkmnn@gmail.com>
2024-06-11 11:55:43 +0900

    refactor: session_cart_idに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,9 +17,9 @@ class ApplicationController < ActionController::Base
   private
 
     def find_or_create_cart
-      cart_id = session[:cart_id]
-      if cart_id.present?
-        Cart.find_by(id: cart_id) || create_cart
+      session_cart_id = session[:cart_id]
+      if session_cart_id.present?
+        Cart.find_by(id: session_cart_id) || create_cart
       else
         create_cart
       end
